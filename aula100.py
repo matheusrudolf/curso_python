4555d94a4a3317f8bcebf8722116acb35b56bdd8
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2026-04-29 14:55:03 -0400

    Continuação das aulas intermediarias de Python

---
+++ aula100.py
@@ -0,0 +1,38 @@
+import copy
+
+from dados import produtos
+
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos produtos por deep copy (cópia profunda)
+novos_produtos = [
+    {**p, "preco": round(p["preco"] * 1.1, 2)} for p in copy.deepcopy(produtos)
+]
+
+# print(*produtos, sep='\n')
+# print()
+# print(*novos_produtos, sep='\n')
+
+# Ordene os produtos por nome decrescente (do maior para o menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos), key=lambda p: p["nome"], reverse=True
+)
+# print(*produtos, sep='\n')
+# print()
+# print(*produtos_ordenados_por_nome, sep='\n')
+
+# Ordene os produtos por preco crescente (do maior para o menor)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+produtos_ordenados_por_preco = sorted(copy.deepcopy(produtos), key=lambda p: p["preco"])
+
+# FINAL
+
+print(*produtos, sep="\n")
+print()
+print(*novos_produtos, sep="\n")
+print()
+print(*produtos_ordenados_por_nome, sep="\n")
+print()
+print(*produtos_ordenados_por_preco, sep="\n")
