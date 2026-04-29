ab755b5dbed9b5243be24633d8d74cf0abdec0f1
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2026-04-29 00:42:34 -0400

    Continuando as aulas intermediarias de Python

---
+++ aula98.py
@@ -0,0 +1,11 @@
+import importlib
+
+import aula98_m
+
+print(aula98_m.variavel)
+
+for i in range(10):
+    print(i)
+    importlib.reload(aula98_m) 
+
+print("Fim")
---
+++ aula99_package/__init__.py
@@ -0,0 +1,6 @@
+# print("Você importou", __name__)
+
+# def dobra(x):
+#     return x * 2
+from .modulo import *
+from .modulo_b import *
