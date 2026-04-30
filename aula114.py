087afd3c48aa89b8b34574f9ac14d010bb2cdae0
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2026-04-30 17:33:05 -0400

    Continuação das aulas intermediarias de Python

--- aula114.py
+++ aula114.py
@@ -7,16 +7,32 @@
 # - Um caso base que para a recursão
 # - fatorial - n! = 5 * 4 * 3 * 2 * 1 = 120
 # https://brasilescola.uol.com.br/matematica/fatorial.htm
-def recursiva(inicio=0, fim=4):
-    print(inicio, fim)
-    # Caso base
-    if inicio >= fim:
-        return fim
+# import sys
+# sys.setrecursionlimit(1004)
+# def recursiva(inicio=0, fim=4):
     
-    # Caso recursivo
-    # contar até chegar ao final
-    inicio += 1
-    return recursiva(inicio, fim)
+#     print(inicio, fim)
+    
+#     # Caso base
+#     if inicio >= fim:
+#         return fim
+    
+#     # Caso recursivo
+#     # contar até chegar ao final
+#     inicio += 1
+#     return recursiva(inicio, fim)
 
 
-print(recursiva())
+# print(recursiva(0, 1001))
+
+def factorial(n):
+    if n <= 1 :
+        return 1
+    
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
+print(factorial(100))
+print(factorial(1000))
