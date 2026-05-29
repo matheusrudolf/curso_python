1441f9582ba2ff5c7abbc63bda1e1f5a72a56b70
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2026-05-29 17:43:43 -0400

    Avançando com aulas de POO em Python

---
+++ aula127_a.py
@@ -0,0 +1,24 @@
+# Exercício - Salve sua classe em JSON
+# Salve os dados da sua classe em JSON
+# e depois crie novamente as instâncias
+# da classe com os dados salvos
+# Faça em arquivos separados
+import json
+
+CAMINHO_ARQUIVO = "aula127.json"
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+p1 = Pessoa("João", 33)
+p2 = Pessoa("Helena", 21)
+p3 = Pessoa("Joana", 11)
+bd = [vars(p1), p2.__dict__, vars(p3)]
+
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO, 'w', encoding='utf8') as arquivo:
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
---
+++ aula127_b.py
@@ -0,0 +1,16 @@
+import json
+
+from aula127_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump
+
+fazer_dump()
+
+with open(CAMINHO_ARQUIVO, 'r', encoding='utf8') as arquivo:
+    pessoas = json.load(arquivo)
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+    p3 = Pessoa(**pessoas[2])
+    
+    print(p1.nome)
+    print(p2.nome)
+    print(p3.nome)
+        
